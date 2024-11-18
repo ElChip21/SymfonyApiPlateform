b799f7f6e6ce9ca6eac3e473a4c76c28d9b58f64
Sclavo Morgan <132832713+ElChip21@users.noreply.github.com>
2024-11-18 12:43:27 +0100

    initialisation du projet, mise en place des entités

---
+++ src/Entity/Album.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\AlbumRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AlbumRepository::class)]
+#[ApiResource]
+class Album
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\ManyToOne(inversedBy: 'albums')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Artiste $artist = null;
+
+    /**
+     * @var Collection<int, Chanson>
+     */
+    #[ORM\OneToMany(targetEntity: Chanson::class, mappedBy: 'album')]
+    private Collection $chansons;
+
+    public function __construct()
+    {
+        $this->chansons = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getArtist(): ?Artiste
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(?Artiste $artist): static
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Chanson>
+     */
+    public function getChansons(): Collection
+    {
+        return $this->chansons;
+    }
+
+    public function addChanson(Chanson $chanson): static
+    {
+        if (!$this->chansons->contains($chanson)) {
+            $this->chansons->add($chanson);
+            $chanson->setAlbum($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChanson(Chanson $chanson): static
+    {
+        if ($this->chansons->removeElement($chanson)) {
+            // set the owning side to null (unless already changed)
+            if ($chanson->getAlbum() === $this) {
+                $chanson->setAlbum(null);
+            }
+        }
+
+        return $this;
+    }
+}
